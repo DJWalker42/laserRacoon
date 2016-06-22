a27159c8e0ba23d0e767255f6ad99dae0fdd0c60
dwalker <darrenwalker1@hotmail.co.uk>
2016-06-22 15:19:35 +0100

    remove logic error when rounding a number to the next decimal order

--- src/FormatOutput.cpp
+++ src/FormatOutput.cpp
@@ -21,22 +21,20 @@ namespace phys{
 			ss << 0 << ".";
 			for (int i = 1; i < sf; ++i) //start at 1 as "0." is already 1 s.f.
 				ss << 0;
+
+			return ss.str();
 		}
-		else if ((abs_n < minSci || abs_n > maxSci)) //anything less than minSci or greater than maxSci is displayed in scientific format
+		
+		int d = int(ceil(log10(abs_n)));
+		double order = pow(10., sf - d);
+		double rn = std::round(n * order) / order;
+		
+		if ((abs_n < minSci || abs_n > maxSci)) //anything less than minSci or greater than maxSci is displayed in scientific format
 		{
-			int d = int(ceil(log10(abs_n)));
-			double order = pow(10, sf - d);
-			ss << std::scientific << std::setprecision(sf - 1) << std::round(order * n) / order;
+			ss << std::scientific << std::setprecision(sf - 1) << rn;
 		}
 		else
 		{
-			int d = int(ceil(log10(abs_n)));
-			double order = pow(10., sf - d);
-
-			double rn = std::round(n * order) / order;
-
-			if (d == int(log10(rn < 0 ? -rn : rn))) ++d; //account for the rounded value being an exact power of ten
-
 			ss << std::fixed << std::setprecision(std::max(sf - d, 0)) << rn;
 		}
 
