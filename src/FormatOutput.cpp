1290636e3624685f204f3bb9b221697858830847
dwalker <darrenwalker1@hotmail.co.uk>
2016-06-22 15:45:27 +0100

    change stl header include

--- src/FormatOutput.cpp
+++ src/FormatOutput.cpp
@@ -1,6 +1,7 @@
-#include <cmath>
-#include <iomanip>
-#include <algorithm>
+#include <sstream>
+#include <cmath> //for pow, log10, and round functions
+#include <iomanip> //for i/o manipulators e.g fixed
+#include <algorithm> //for max function
 
 #include "FormatOutput.h"
 
@@ -16,8 +17,7 @@ namespace phys{
 
 		double abs_n = fabs(n);
 
-		if (abs_n <= zeroThresh)
-		{
+		if (abs_n <= zeroThresh){
 			ss << 0 << ".";
 			for (int i = 1; i < sf; ++i) //start at 1 as "0." is already 1 s.f.
 				ss << 0;
@@ -29,12 +29,11 @@ namespace phys{
 		double order = pow(10., sf - d);
 		double rn = std::round(n * order) / order;
 		
-		if ((abs_n < minSci || abs_n > maxSci)) //anything less than minSci or greater than maxSci is displayed in scientific format
-		{
+		//anything less than minSci or greater than maxSci is displayed in scientific format
+		if ((abs_n < minSci || abs_n > maxSci)) { 
 			ss << std::scientific << std::setprecision(sf - 1) << rn;
 		}
-		else
-		{
+		else {
 			ss << std::fixed << std::setprecision(std::max(sf - d, 0)) << rn;
 		}
 
